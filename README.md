3bacfc5cbaa132650461a278c69acc67c5675fd7
verizoncoins <verizoncoin@gmail.com>
2019-05-02 18:16:54 +0900

    init

---
+++ README.md
@@ -0,0 +1,165 @@
+UniNet Core integration/staging repository
+=====================================
+
+Uninet is a decentralized insurance marketplace bringing equitable price discovery and global access to insurance policy shoppers worldwide. Uninet puts consumer power back in your hands with an honest, accountable, and democratic insurance marketplace on the blockchain.
+- Decentralized blockchain voting providing for consensus based advancement of the current Masternode technology used to secure the network and provide the above features, each Masternode is secured with collateral of 1000 UNN
+- Anonymized transactions using coin mixing technology, we call it _Privatesend_.
+- Fast transactions featuring guaranteed zero confirmation transactions, we call it _InstantSend_.
+
+More information at [unn.io](http://uninet-coin.com)
+
+### Coin Specs
+<table>
+<tr><td>Type</td><td>POS + Masternode</td></tr>
+<tr><td>Algo</td><td>Quark</td></tr>
+<tr><td>Block Time</td><td>60 Seconds</td></tr>
+<tr><td>Maturity</td><td>100 Confirmations</td></tr>
+<tr><td>Difficulty Retargeting</td><td>Every Block</td></tr>
+<tr><td>Masternode Collateral</td><td>1,000 UNN</td></tr>
+<tr><td>P2P port</td><td>47578</td></tr>
+<tr><td>RPC port</td><td>47579</td></tr>
+<tr><td>Max Coin Supply</td><td>21,000,000 UNN</td></tr>
+<tr><td>Premine</td><td>5% *</td></tr>
+</table>
+
+*View Coin Distribution on [unn.io](http://uninet-coin.com)
+
+### Reward Distribution
+
+
+<table>
+<thead>
+<tr>
+<th scope="col">BLock no</th>
+<th scope="col">Reward / block</th>
+<th scope="col">Staking</th>
+<th scope="col">Masternodes</th> 
+</tr>
+</thead>
+<tbody>
+<tr>
+<th scope="row">1-21600</th>
+<th scope="row">0.5 UNN</th>
+<td>0.10 UNN (20%)</td>
+<td>0.40 UNN (80%)</td> 
+</tr>
+<tr>
+<th scope="row">21601-43200</th>
+<th scope="row">1 UNN</th>
+<td>0.20 UNN (20%)</td>
+<td>0.80 UNN (80%)</td>
+</tr>
+<tr>
+<th scope="row">43201-64800</th>
+<th scope="row">1.5 UNN</th>
+<td>0.30 UNN (20%)</td>
+<td>1.20 UNN (80%)</td>
+</tr>
+<tr>
+<th scope="row">64801-87840</th>
+<th scope="row">2 UNN</th>
+<td>0.40 UNN (20%)</td>
+<td>1.60 UNN (80%)</td>
+</tr>
+<tr>
+<th scope="row">87841-106560</th>
+<th scope="row">2.5 UNN</th>
+<td>0.37 UNN (15%)</td>
+<td>2.12 UNN (85%)</td>
+</tr>
+<tr>
+<th scope="row">106561-129600</th>
+<th scope="row">3 UNN</th>
+<td>0.45 UNN (15%)</td>
+<td>2.55 UNN (85%)</td>
+</tr>
+<tr>
+<th scope="row">129601-151200</th>
+<th scope="row">3.5 UNN</th>
+<td>0.52 UNN (15%)</td>
+<td>2.97 UNN (85%)</td>
+</tr>
+<tr>
+<th scope="row">151201-172800</th>
+<th scope="row">4 UNN</th>
+<td>0.60 UNN (15%)</td>
+<td>3.40 UNN (85%)</td>
+</tr>
+<tr>
+<th scope="row">172801-194400</th>
+<th scope="row">4.5 UNN</th>
+<td>0.45 UNN (10%)</td>
+<td>4.05 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">194401-216000</th>
+<th scope="row">5 UNN</th>
+<td>0.50 UNN (10%)</td>
+<td>4.50 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">216001-237600</th>
+<th scope="row">5.5 UNN</th>
+<td>0.55 UNN (10%)</td>
+<td>4.95 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">237601-259200</th>
+<th scope="row">6 UNN</th>
+<td>0.60 UNN (10%)</td>
+<td>5.40 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">259201-280800</th>
+<th scope="row">6.5 UNN</th>
+<td>0.65 UNN (10%)</td>
+<td>5.85 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">280801-302400</th>
+<th scope="row">7 UNN</th>
+<td>0.70 UNN (10%)</td>
+<td>6.30 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">302401-324000</th>
+<th scope="row">7.5 UNN</th>
+<td>0.75 UNN (10%)</td>
+<td>6.75 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">324001-345600</th>
+<th scope="row">8 UNN</th>
+<td>0.8 0SINS (10%)</td>
+<td>7.20 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">345601-367200</th>
+<th scope="row">8.5 UNN</th>
+<td>0.85 UNN (10%)</td>
+<td>7.65 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">367201-388800</th>
+<th scope="row">9 UNN</th>
+<td>0.90 UNN (10%)</td>
+<td>8.10 UNN (90%)</td>
+</tr>
+<tr>
+<th scope="row">388801-525600</th>
+<th scope="row">5 UNN</th>
+<td>1 UNN (20%)</td>
+<td>4 UNN (80%)</td>
+</tr>
+<tr>
+<tr>
+<th scope="row">525601-1,051,200</th>
+<th scope="row">4 UNN</th>
+<td>0.8 UNN (20%)</td>
+<td>3.2 UNN (80%)</td>
+</tr>
+<tr>						
+<th scope="row" colspan=4>After 2 years, each year, the reward will be reduced with 10%</th>
+</tr>
+</tbody>
+</table>
---
+++ src/chainparams.cpp
@@ -0,0 +1,444 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2014-2015 The Dash developers
+// Copyright (c) 2015-2017 The PIVX developers
+// Copyright (c) 2017-2019 The Unn Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "chainparams.h"
+
+#include "random.h"
+#include "util.h"
+#include "utilstrencodings.h"
+
+#include <assert.h>
+
+#include <boost/assign/list_of.hpp>
+
+using namespace std;
+using namespace boost::assign;
+
+struct SeedSpec6 {
+    uint8_t addr[16];
+    uint16_t port;
+};
+
+#include "chainparamsseeds.h"
+
+/**
+ * Main network
+ */
+
+
+static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data, unsigned int count)
+{
+    const int64_t nOneWeek = 7 * 24 * 60 * 60;
+    for (unsigned int i = 0; i < count; i++) {
+        struct in6_addr ip;
+        memcpy(&ip, data[i].addr, sizeof(ip));
+        CAddress addr(CService(ip, data[i].port));
+        addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+
+static Checkpoints::MapCheckpoints mapCheckpoints =
+    boost::assign::map_list_of
+    (0, uint256("0x000003935675d1f44610067ba64a593e84fb0fc200c4d709f4d785f178b9930e"))
+    ;
+
+static const Checkpoints::CCheckpointData data = {
+    &mapCheckpoints,
+    1556485989 ,  // * UNIX timestamp of last checkpoint block
+    0,      // * total number of transactions between genesis and last checkpoint
+                 //   (the tx=... number in the SetBestChain debug.log lines)
+    2000         // * estimated number of transactions per day after checkpoint
+};
+
+static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
+    boost::assign::map_list_of(0, uint256("0x00000563e49224a713a16adf2e08fd0e91625cbe2dadd2de9b68acc7e71ecc79"));
+static const Checkpoints::CCheckpointData dataTestnet = {
+    &mapCheckpointsTestnet,
+    1555496931,
+    0,
+    250};
+
+static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
+    boost::assign::map_list_of(0, uint256("0x4cd52384d1ca10442612b097910772c5c3a15289f55bf9e09626d9421fc991f2"));
+static const Checkpoints::CCheckpointData dataRegtest = {
+    &mapCheckpointsRegtest,
+    1555496977,
+    0,
+    100};
+
+class CMainParams : public CChainParams
+{
+public:
+    CMainParams()
+    {
+        networkID = CBaseChainParams::MAIN;
+        strNetworkID = "main";
+        pchMessageStart[0] = 0x4a;
+        pchMessageStart[1] = 0x4f;
+        pchMessageStart[2] = 0x13;
+        pchMessageStart[3] = 0x70;
+        vAlertPubKey = ParseHex("040a1ade1eed7451e9d8f45193bc3d00d6039d955215af9671d311a9097bfc2d83f2df32d49b920511bf912f3f13da36c481359acec29944d3965c26da1fe2f6eb");
+        nDefaultPort = 47578;
+        bnProofOfWorkLimit = ~uint256(0) >> 20; // Unn starting difficulty is 1 / 2^12
+        nSubsidyHalvingInterval = 525600; // one year
+        nMaxReorganizationDepth = 100;
+        nEnforceBlockUpgradeMajority = 750;
+        nRejectBlockOutdatedMajority = 950;
+        nToCheckBlockUpgradeMajority = 1000;
+        nMinerThreads = 0;
+        nTargetTimespan = 1 * 60; // Unn: every block
+        nTargetSpacing = 1 * 60;  // Unn: 1 minute
+        nLastPOWBlock = 200;
+        nMaturity = 20;
+        nMasternodeCountDrift = 20;
+        nModifierUpdateBlock = 1;
+        nMaxMoneyOut = 5000000 * COIN;
+        nMasternodeCollateral = 1000; 
+
+        const char* pszTimestamp = "Happybirth Uninet Change network 2019-04-26";
+        CMutableTransaction txNew;
+        txNew.vin.resize(1);
+        txNew.vout.resize(1);
+        txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        txNew.vout[0].nValue = 50 * COIN;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("0464d798bc68345552dbcdd97258985ec56acf377f84b740ceb35c292bd69be37afc8494251e8b80c70c1aa1f0908ee6f9002b7241317c49ed3b45714eb398daf5") << OP_CHECKSIG;
+        genesis.vtx.push_back(txNew);
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime = 1556485989 ;
+        genesis.nBits = 0x1e0ffff0;
+        genesis.nNonce = 5110889;
+
+        // if(genesis.GetHash() != uint256("0x"))
+        // {
+        //     printf("MSearching for genesis block...\n");
+        //     uint256 hashTarget;
+        //     hashTarget.SetCompact(genesis.nBits);
+        //     while(uint256(genesis.GetHash()) > uint256(hashTarget))
+        //     {
+        //         ++genesis.nNonce;
+        //         if (genesis.nNonce == 0)
+        //         {
+        //             printf("Mainnet NONCE WRAPPED, incrementing time");
+        //             std::cout << std::string("Mainnet NONCE WRAPPED, incrementing time:\n");
+        //             ++genesis.nTime;
+        //         }
+        //         if (genesis.nNonce % 10000 == 0)
+        //         {
+        //         printf("Mainnet: nonce %08u: hash = %s \n", genesis.nNonce, genesis.GetHash().ToString().c_str());
+        //         }
+        //     }
+        //     printf("Mainnet block.nTime = %u \n", genesis.nTime);
+        //     printf("Mainnet block.nNonce = %u \n", genesis.nNonce);
+        //     printf("Mainnet block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+        //     printf("Mainnet block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+        // }
+
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("0x000003935675d1f44610067ba64a593e84fb0fc200c4d709f4d785f178b9930e"));
+        assert(genesis.hashMerkleRoot == uint256("0x9440deac67f8cb0848041b1b2b884c13a76a120b5f47ce959ddcd84cb097848f"));
+
+        vSeeds.push_back(CDNSSeedData("dns1.uninet-coin.com", "dns1.uninet-coin.com"));
+        vSeeds.push_back(CDNSSeedData("dns2.uninet-coin.com", "dns2.uninet-coin.com"));
+        vSeeds.push_back(CDNSSeedData("dns3.uninet-coin.com", "dns3.uninet-coin.com"));
+        vSeeds.push_back(CDNSSeedData("ns1.uninet-coin.com", "ns1.uninet-coin.com"));
+        vSeeds.push_back(CDNSSeedData("ns2.uninet-coin.com", "ns2.uninet-coin.com"));
+        vSeeds.push_back(CDNSSeedData("ns3.uninet-coin.com", "ns3.uninet-coin.com"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 68);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 33);
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 212);
+		// Unn BIP32 pubkeys start with 'xpub' (Bitcoin defaults)
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x02)(0x2F)(0x25)(0x43).convert_to_container<std::vector<unsigned char> >();
+		// Unn BIP32 pubkeys start with 'xprv' (Bitcoin defaults)		
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x02)(0x22)(0x31)(0x2C).convert_to_container<std::vector<unsigned char> >();
+        // 	BIP44 coin type is '0X80000a00' from https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x08)(0x57).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
+
+        fRequireRPCPassword = true;
+        fMiningRequiresPeers = true;
+        fAllowMinDifficultyBlocks = false;
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = true;
+        fMineBlocksOnDemand = false;
+        fSkipProofOfWorkCheck = false;
+        fTestnetToBeDeprecatedFieldRPC = false;
+        fHeadersFirstSyncingActive = false;
+
+        nPoolMaxTransactions = 3;
+        strSporkKey = "0489abad26629c093ec07eca7ea0718ab91f99dca3a1a51cc7d96bf2b5581c82c5425fedf61539125b301343e5e86ff7ff566918079bd63d34319b67dff6e2f1fb";
+        strPrivatesendPoolDummyAddress = "Ugg1xjNjXrNcGxAg6ca64BSLEZKJWvt4Hv";
+        nStartMasternodePayments = 1556485990;
+    }
+
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return data;
+    }
+};
+static CMainParams mainParams;
+
+/**
+ * Testnet (v3)
+ */
+class CTestNetParams : public CMainParams
+{
+public:
+    CTestNetParams()
+    {
+        networkID = CBaseChainParams::TESTNET;
+        strNetworkID = "test";
+        pchMessageStart[0] = 0x25;
+        pchMessageStart[1] = 0x1c;
+        pchMessageStart[2] = 0x5a;
+        pchMessageStart[3] = 0x76;
+        vAlertPubKey = ParseHex("043a669e18620573c743859bf127a74481d01cb79b53b3a418fd31bc22854f044ad8da1fab2a468430e8b2ca2f72c9eb665b554dab1cee611bbd25b24f65d9c643");
+        nDefaultPort = 51578;
+        nEnforceBlockUpgradeMajority = 51;
+        nRejectBlockOutdatedMajority = 75;
+        nToCheckBlockUpgradeMajority = 100;
+        nMinerThreads = 0;
+        nTargetTimespan = 1 * 60; // Unn: 1 day
+        nTargetSpacing = 1 * 60;  // Unn: 1 minute
+        nLastPOWBlock = 200;
+        nMaturity = 15;
+        nMasternodeCountDrift = 4;
+        nModifierUpdateBlock = 1; //approx Mon, 17 Apr 2017 04:00:00 GMT
+        nMaxMoneyOut = 43199500 * COIN;
+
+        //! Modify the testnet genesis block so the timestamp is valid for a later start.
+        genesis.nTime = 1555496931;
+        genesis.nNonce = 11973825;
+
+        // if(genesis.GetHash() != uint256("0x"))
+        // {
+        //     printf("MSearching for genesis block...\n");
+        //     uint256 hashTarget;
+        //     hashTarget.SetCompact(genesis.nBits);
+        //     while(uint256(genesis.GetHash()) > uint256(hashTarget))
+        //     {
+        //         ++genesis.nNonce;
+        //         if (genesis.nNonce == 0)
+        //         {
+        //             printf("TestNet NONCE WRAPPED, incrementing time");
+        //             std::cout << std::string("TestNet NONCE WRAPPED, incrementing time:\n");
+        //             ++genesis.nTime;
+        //         }
+        //         if (genesis.nNonce % 10000 == 0)
+        //         {
+        //         printf("TestNet: nonce %08u: hash = %s \n", genesis.nNonce, genesis.GetHash().ToString().c_str());
+        //         }
+        //     }
+        //     printf("TestNet block.nTime = %u \n", genesis.nTime);
+        //     printf("TestNet block.nNonce = %u \n", genesis.nNonce);
+        //     printf("TestNet block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+        //     printf("TestNet block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+        // }
+
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("0x00000563e49224a713a16adf2e08fd0e91625cbe2dadd2de9b68acc7e71ecc79"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 80); // Testnet unn addresses start with 'x' or 'y'
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 20);  // Testnet unn script addresses start with '8' or '9'
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 240);     // Testnet private keys start with '9' or 'c' (Bitcoin defaults)
+		// Unn BIP32 pubkeys start with 'xpub' (Bitcoin defaults)
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x3a)(0x80)(0x61)(0xa0).convert_to_container<std::vector<unsigned char> >();
+		// Unn BIP32 pubkeys start with 'xprv' (Bitcoin defaults)		
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x3a)(0x80)(0x58)(0x37).convert_to_container<std::vector<unsigned char> >();
+        // Testnet unn BIP44 coin type is '1' (All coin's testnet default)
+        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0x01).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
+
+        fRequireRPCPassword = true;
+        fMiningRequiresPeers = true;
+        fAllowMinDifficultyBlocks = true;
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = true;
+        fMineBlocksOnDemand = false;
+        fTestnetToBeDeprecatedFieldRPC = true;
+
+        nPoolMaxTransactions = 2;
+        strSporkKey = "04a15e2316b48af3387763d2916d0cda48c464c23ace1719484187c00db9000eb26d3da09326408b139496783237f298feb40efdf094cfdb235ccd7b3620e71414";
+        strPrivatesendPoolDummyAddress = "ZPo9bLZPdwSoyKozFEomY9VK6FXoaM4i84";
+        nStartMasternodePayments = 1537178400;
+    }
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return dataTestnet;
+    }
+};
+static CTestNetParams testNetParams;
+
+/**
+ * Regression test
+ */
+class CRegTestParams : public CTestNetParams
+{
+public:
+    CRegTestParams()
+    {
+        networkID = CBaseChainParams::REGTEST;
+        strNetworkID = "regtest";
+        pchMessageStart[0] = 0xa1;
+        pchMessageStart[1] = 0xcf;
+        pchMessageStart[2] = 0x7e;
+        pchMessageStart[3] = 0xac;
+        nSubsidyHalvingInterval = 150;
+        nEnforceBlockUpgradeMajority = 750;
+        nRejectBlockOutdatedMajority = 950;
+        nToCheckBlockUpgradeMajority = 1000;
+        nMinerThreads = 1;
+        nTargetTimespan = 24 * 60 * 60; // Unn: 1 day
+        nTargetSpacing = 1 * 60;        // Unn: 1 minutes
+        bnProofOfWorkLimit = ~uint256(0) >> 1;
+        genesis.nTime = 1555496977;
+        genesis.nBits = 0x207fffff;
+        genesis.nNonce = 7722705;
+
+        // if(genesis.GetHash() != uint256("0x"))
+        // {
+        //     printf("MSearching for genesis block...\n");
+        //     uint256 hashTarget;
+        //     hashTarget.SetCompact(genesis.nBits);
+        //     while(uint256(genesis.GetHash()) > uint256(hashTarget))
+        //     {
+        //         ++genesis.nNonce;
+        //         if (genesis.nNonce == 0)
+        //         {
+        //             printf("regtest NONCE WRAPPED, incrementing time");
+        //             std::cout << std::string("regtest NONCE WRAPPED, incrementing time:\n");
+        //             ++genesis.nTime;
+        //         }
+        //         if (genesis.nNonce % 10000 == 0)
+        //         {
+        //         printf("regtest: nonce %08u: hash = %s \n", genesis.nNonce, genesis.GetHash().ToString().c_str());
+        //         }
+        //     }
+        //     printf("regtest block.nTime = %u \n", genesis.nTime);
+        //     printf("regtest block.nNonce = %u \n", genesis.nNonce);
+        //     printf("regtest block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+        //     printf("regtest block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+        // }
+
+        hashGenesisBlock = genesis.GetHash();
+        nDefaultPort = 52578;
+        assert(hashGenesisBlock == uint256("0x4cd52384d1ca10442612b097910772c5c3a15289f55bf9e09626d9421fc991f2"));
+
+        vFixedSeeds.clear(); //! Testnet mode doesn't have any fixed seeds.
+        vSeeds.clear();      //! Testnet mode doesn't have any DNS seeds.
+
+        fRequireRPCPassword = false;
+        fMiningRequiresPeers = false;
+        fAllowMinDifficultyBlocks = true;
+        fDefaultConsistencyChecks = true;
+        fRequireStandard = true;
+        fMineBlocksOnDemand = false;
+        fTestnetToBeDeprecatedFieldRPC = false;
+    }
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return dataRegtest;
+    }
+};
+static CRegTestParams regTestParams;
+
+/**
+ * Unit test
+ */
+class CUnitTestParams : public CMainParams, public CModifiableParams
+{
+public:
+    CUnitTestParams()
+    {
+        networkID = CBaseChainParams::UNITTEST;
+        strNetworkID = "unittest";
+        nDefaultPort = 53578;
+        vFixedSeeds.clear(); //! Unit test mode doesn't have any fixed seeds.
+        vSeeds.clear();      //! Unit test mode doesn't have any DNS seeds.
+
+        fRequireRPCPassword = false;
+        fMiningRequiresPeers = false;
+        fDefaultConsistencyChecks = true;
+        fAllowMinDifficultyBlocks = false;
+        fMineBlocksOnDemand = true;
+    }
+
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        // UnitTest share the same checkpoints as MAIN
+        return data;
+    }
+
+    //! Published setters to allow changing values in unit test cases
+    virtual void setSubsidyHalvingInterval(int anSubsidyHalvingInterval) { nSubsidyHalvingInterval = anSubsidyHalvingInterval; }
+    virtual void setEnforceBlockUpgradeMajority(int anEnforceBlockUpgradeMajority) { nEnforceBlockUpgradeMajority = anEnforceBlockUpgradeMajority; }
+    virtual void setRejectBlockOutdatedMajority(int anRejectBlockOutdatedMajority) { nRejectBlockOutdatedMajority = anRejectBlockOutdatedMajority; }
+    virtual void setToCheckBlockUpgradeMajority(int anToCheckBlockUpgradeMajority) { nToCheckBlockUpgradeMajority = anToCheckBlockUpgradeMajority; }
+    virtual void setDefaultConsistencyChecks(bool afDefaultConsistencyChecks) { fDefaultConsistencyChecks = afDefaultConsistencyChecks; }
+    virtual void setAllowMinDifficultyBlocks(bool afAllowMinDifficultyBlocks) { fAllowMinDifficultyBlocks = afAllowMinDifficultyBlocks; }
+    virtual void setSkipProofOfWorkCheck(bool afSkipProofOfWorkCheck) { fSkipProofOfWorkCheck = afSkipProofOfWorkCheck; }
+};
+static CUnitTestParams unitTestParams;
+
+
+static CChainParams* pCurrentParams = 0;
+
+CModifiableParams* ModifiableParams()
+{
+    assert(pCurrentParams);
+    assert(pCurrentParams == &unitTestParams);
+    return (CModifiableParams*)&unitTestParams;
+}
+
+const CChainParams& Params()
+{
+    assert(pCurrentParams);
+    return *pCurrentParams;
+}
+
+CChainParams& Params(CBaseChainParams::Network network)
+{
+    switch (network) {
+    case CBaseChainParams::MAIN:
+        return mainParams;
+    case CBaseChainParams::TESTNET:
+        return testNetParams;
+    case CBaseChainParams::REGTEST:
+        return regTestParams;
+    case CBaseChainParams::UNITTEST:
+        return unitTestParams;
+    default:
+        assert(false && "Unimplemented network");
+        return mainParams;
+    }
+}
+
+void SelectParams(CBaseChainParams::Network network)
+{
+    SelectBaseParams(network);
+    pCurrentParams = &Params(network);
+}
+
+bool SelectParamsFromCommandLine()
+{
+    CBaseChainParams::Network network = NetworkIdFromCommandLine();
+    if (network == CBaseChainParams::MAX_NETWORK_TYPES)
+        return false;
+
+    SelectParams(network);
+    return true;
+}
